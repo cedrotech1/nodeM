ecf69d0fce978c4534196075da4663e1e68abcef
Cedrick <cedrotech1@gmail.com>
2023-07-01 04:53:36 -0700

    changes

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express=require("express");
-const skills=require("./models/skills");
+// const skills=require("./models/skills");
 const app=express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// const { Schema } = mongoose;
+app.use(express.json());
 
+// const app=express();
+// app.use(express.json());
+// const mongoose = require('mongoose');
 // Require the route file
 const skillsRoute = require('./route/skills');
 
 // Use the route middleware
 app.use('/skills', skillsRoute);
 
-const mongoose = require('mongoose');
 
- // MongoClient.connect("mongodb+srv://cedrick:cedrick@cluster0.wtzj3ht.mongodb.net/?retryWrites=true&w=majority")
+
 const uri = 'mongodb://127.0.0.1:27017/protofolio';
+ 
+// const uri = 'mongodb+srv://cedrick:cedrick@cluster0.wtzj3ht.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
--- route/skills.js
+++ route/skills.js
@@ -1,27 +1,110 @@
 const express = require('express');
 const router = express.Router();
+const skills=require("../models/skills");
+// const body = require('body-parser');
 
 // Define a route
 // router.get('/', (req, res) => {
 //   res.send('Hello, World!');
-// });
-router.get('/add', (req, res) => {
-    const oneskills = new skills({
-        title: 'java',
-        // age: 25,
-        rate: '78'
-      });
-      
-      // Save the document
+// });   
+
+
+
+  router.post('/add', (req, res) => {
+    const title=req.body.title;
+    const rate=req.body.rate;
+    const data={title,rate}
+    
+    const oneskills = new skills(data);
+
       oneskills.save()
-        .then((doc) => {
-          console.log('Saved document:', doc);
+        .then(() => {
+        res.json(oneskills)
         })
         .catch((err) => {
           console.error('Error saving document:', err);
         })
     
   })
+
+
+  router.get('/view', async (req, res) => {
+    try {
+         const data= await skills.find().limit(7);
+         const total=data.length;
+        //  console.log(total)
+
+         if(total>2)
+         {
+            res.send({data:data,total:total})
+
+         }else{
+            res.send({message:"it above limited"})
+         }
+
+     
+    
+    } catch (error) {
+        res.send(error)
+    }
+    
+  })
+
+  router.get('/one/:id', async (req, res) => {
+    try {
+      const id = req.params.id;
+      const data = await skills.findById(id);
+
+      if (!data) {
+        // Data does not exist for the provided id
+        return res.send('Data not found');
+      }
+      // Process the retrieved data
+      res.send(data);
+    } catch (error) {
+      console.error('Error retrieving data:', error);
+      res.status(500).send('Error retrieving data');
+    }
+  });
+  
+
+ router.put('/update/:id', (req, res) => {
+   try {
+
+    const id=req.params.id;
+
+    const title=req.body.title;
+    const rate=req.body.rate;
+    const data={title,rate}
+    
+    // const oneskills = new skills(data);
+    skills.findByIdAndUpdate(id,data)
+            .then(() => {
+                // Process the retrieved data
+                res.send(data)
+                })
+                .catch((error) => {
+                console.error('Error retrieving data:', error);
+                }); 
+    
+   } catch (error) {
+    console.log(error)
+   } 
+ })
+
+ router.delete("/delete/:id",(req,res)=>{
+    try {
+        const id=req.params.id
+        skills.findByIdAndDelete(id)
+        .then(()=>{
+            res.status(200).json("well deleted")
+        })
+        
+    } catch (error) {
+        res.status(500).json({error:error})
+    }
+ })
+
 
 // Export the router
 module.exports = router;

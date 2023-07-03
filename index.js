f24d56f782c1e4a37a11052c84261f1545150ec8
Cedrick <cedrotech1@gmail.com>
2023-07-03 18:22:35 +0200

    comit....

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const mongoose = require('mongoose');
 // const { Schema } = mongoose;
 app.use(express.json());
 
-const connectionString ="mongodb+srv://cedrick:cedrick@cluster0.wtzj3ht.mongodb.net/?retryWrites=true&w=majority";
+const uri = 'mongodb://127.0.0.1:27017/protofolio';
+
+// const uri ="mongodb+srv://cedrick:cedrick@cluster0.wtzj3ht.mongodb.net/?retryWrites=true&w=majority";
 // const connectionString = 'mongodb+srv://cedrick:cedrick@cluster0.wtzj3ht.mongodb.net/?retryWrites=true&w=majority';
 // const mongoose = require('mongoose');
 // const connectionString = 'your-mongodb-connection-string'; // Replace with your actual connection string
 
-mongoose.connect(connectionString, {
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -40,9 +42,12 @@ app.use('/room', roomRoute);
 
 // const uri = 'mongodb://127.0.0.1:27017/protofolio';
 // cedrick:<password>@cluster0.wtzj3ht.mongodb.net/?retryWrites=true&w=majority
+const x={
+  wellcame:"welcame mr"
+}
 
   app.get('/', (req, res) => {
-    res.send('Hello, Cedrick test....!');
+    res.json({message:x.wellcame});
   });
   
 
